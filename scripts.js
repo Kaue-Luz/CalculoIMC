96c418eb0b1acc1d9f73590fa78519d78a750dca
Erik Kauê <kaue_erik122@hotmail.com>
2023-08-20 19:40:18 -0300

    Showing the IMC in the popup

--- scripts.js
+++ scripts.js
@@ -3,11 +3,32 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+const modalWrraper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal-wrapper .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+
+
 form.onsubmit = function (event) {
     event.preventDefault() // evitar padrão 
 
     const weight = inputWeight.value
     const height = inputHeight.value
+ 
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
 
-    
+    modalMessage.innerText = message
+    modalWrraper.classList.add('open')
 }
+
+modalBtnClose.onclick = function() {
+    modalWrraper.classList.remove('open')
+}
+
+//Calculo IMC
+// Elevar numero (**)
+function IMC(weight, height){
+    return (weight / ((height / 100) **2 )).toFixed(2)
+}
+
