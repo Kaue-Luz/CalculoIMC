78f9ec9385acaff4cd99af088a8a5660b0708dd2
Erik Kauê <kaue_erik122@hotmail.com>
2023-08-21 17:01:53 -0300

    Minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@ import { Modal } from "./modal.js"
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+const alertError = document.querySelector('.alert-error')
 
 //const modalWrraper = document.querySelector('.modal-wrapper')
 //const modalMessage = document.querySelector('.modal-wrapper .title span')
 //const modalBtnClose = document.querySelector('.modal button.close')
+
+inputWeight.oninput = () => alertError.classList.remove('open')
+inputHeight.oninput = () => alertError.classList.remove('open')
 
 form.onsubmit = function (event) {
     event.preventDefault() // evitar padrão 
@@ -16,6 +20,12 @@ form.onsubmit = function (event) {
     const height = inputHeight.value
  
     const result = IMC(weight, height)
+
+    //Mensagem de erro
+    if(!result){
+       return alertError.classList.add('open')
+    }
+
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
@@ -26,6 +36,8 @@ form.onsubmit = function (event) {
 //Calculo IMC
 // Elevar numero (**)
 function IMC(weight, height){
-    return (weight / ((height / 100) **2 )).toFixed(2)
+    if (Number(weight) && Number(height)) {
+        return (weight / ((height / 100) **2 )).toFixed(2)
+    }
+    return false
 }
-
